352336e259db163741096a015204b9a67bb28f40
IgorMushk <igor.mushketik@gmail.com>
2023-12-02 15:00:52 +0200

    add animalSchema

--- src/modules/animals/models/animal.js
+++ src/modules/animals/models/animal.js
@@ -1,16 +1,27 @@
-const {nanoid} = require('nanoid');
+//const {nanoid} = require('nanoid');
+const {Schema, default: mongoose} = require("mongoose");
 
-class Animal {
-    constructor(payload) {
-        this.id = nanoid();
-        this.createdAt = new Date().toISOString();
-        this.updateAt = new Date().toISOString();
-        this.name = payload.name;
-        this.age = payload.age;
-        this.isVaccinated = payload.isVaccinated;
-        this.gender = payload.gender;
-        this.species = payload.species;
-    }
-}
+// class AnimalOld {
+//     constructor(payload) {
+//         this.id = nanoid();
+//         this.createdAt = new Date().toISOString();
+//         this.updateAt = new Date().toISOString();
+//         this.name = payload.name;
+//         this.age = payload.age;
+//         this.isVaccinated = payload.isVaccinated;
+//         this.gender = payload.gender;
+//         this.species = payload.species;
+//     }
+// }
 
-module.exports = Animal;
+const animalSchema = new Schema({
+  name: String,
+  age: Number,
+  isVaccinated: {type: Boolean, default: false},
+  gender: {type: String, enum: ["male", "famaile"]},
+  species: String,
+}, {versionKey: false, timestamps: true})
+
+const Animal = mongoose.model("animals", animalSchema);
+
+// module.exports = AnimalOld;
